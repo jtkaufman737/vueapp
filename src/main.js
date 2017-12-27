0608fbfdada8aaf7f122adf563ed1f113eea4bbd
Joy <jtkaufman737@gmail.com>
2017-12-27 15:07:26 -0500

    Hooking up create component file added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
+import CreateItem from './components/CreateItem.vue';
 Vue.use(VueRouter);
 
 import VueAxios from 'vue-axios';
